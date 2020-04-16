73d1d281bda38ed5fa1c39536707b52d8d368ae4
Peter PJT <pjtsignups@gmail.com>
2020-04-15 18:08:13 -0700

    have panel close on workspace change, cancel panel events on panel close

--- src/utilities/panels.js
+++ src/utilities/panels.js
@@ -6,14 +6,28 @@ export const create = ({API})=> {
     var newIds = getIds()
     var id = newIds.find((newId)=>!oldIds.some((oldId)=> oldId === newId));
     var panelElement = document.getElementById(id)
-    API.RunningConfig.on('aTabHasBeenClosed',function({tabElement}){
+    var events = []
+
+    events.push(API.RunningConfig.on('aTabHasBeenClosed',function({tabElement}){
         if (tabElement.getAttribute("classselector").includes(id) && panelElement.querySelector(`.tabsbar`).children.length === 0){
-            remove({id,RunningConfig:API.RunningConfig})
+            remove({id,RunningConfig:API.RunningConfig,events})
         }
-	})
+    }))
+    events.push(API.RunningConfig.on('setWorkspace',()=>{
+      console.log(id)
+      remove({id,RunningConfig:API.RunningConfig,events})
+    }))
+    events.push(API.RunningConfig.on('addFolderToRunningWorkspace',()=>{
+      console.log(id)
+      remove({id,RunningConfig:API.RunningConfig,events})
+    }))
+    console.log(API.RunningConfig)
     return panelElement
 }
-export const remove = ({id,RunningConfig}) => {
-    document.getElementById(id).remove()
-    RunningConfig.data.focusedPanel = document.querySelector("#mainpanel").children[0]
+export const remove = ({id,RunningConfig,events}) => {
+    if (document.getElementById(id)){
+      document.getElementById(id).remove()
+      RunningConfig.data.focusedPanel = document.querySelector("#mainpanel").children[0]
+      if (events) events.forEach(event=>event.cancel());
+    }
 }
