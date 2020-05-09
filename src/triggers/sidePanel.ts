9680ceccb27637672ff68f5bf138748a8c0f1198
Peter PJT <pjtsignups@gmail.com>
2020-05-08 19:28:36 -0700

    feat(SidePanelIcon): add SidePanelIcon

--- src/triggers/sidePanel.ts
+++ src/triggers/sidePanel.ts
@@ -1,10 +1,13 @@
 import { SidePanel } from "../actions/index"
+import { SidePanelIcon } from "../actions/SidePanel/SidePanelIcon"
 
 export const register = ({ API, options }: { API: any; options: any }) => {
   const sidePanel = SidePanel({ API })
+  const sidePanelIcon = SidePanelIcon({ API })
   API.SidePanel({
-    icon: () => API.puffin.element`<span style="color:white">G</span>`,
+    icon: sidePanelIcon.component,
     panel: sidePanel.component,
   })
   sidePanel.render()
+  sidePanelIcon.render()
 }
